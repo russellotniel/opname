745ecb703d73d0071a0f417649f9fe72687c3fdd
russellotniel <russellotniel@gmail.com>
2023-01-06 09:24:41 +0700

    minor fixes and deleted landing page file in pages

--- components/layout/MobileLayout.js
+++ components/layout/MobileLayout.js
@@ -1,6 +1,7 @@
 const MobileLayout = (props) => {
 	return (
-		<div className='border-solid border-2 border-black rounded-md bg-white' style={{ width: '414px', height: '896px',}}>
+		<div className='border-solid border-2 border-black rounded-md bg-white' style={{ width: '414px', height: '95vh', overflowY: 'auto' }}>
+			{/* <div className='border-solid border-2 border-black rounded-md bg-white' style={{ width: '414px', height: '896px' }}> */}
 			{props.children}
 		</div>
 	);
